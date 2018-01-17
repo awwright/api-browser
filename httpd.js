2019973dc3e3eec6730c535c0bcdba8b29e4d8fb
Austin Wright <aaa@bzfx.net>
2018-01-17 08:51:47 -0700

    Move Document render function to lib/

--- httpd.js
+++ httpd.js
@@ -1,20 +1,23 @@
 
 var http = require('http');
+var qs = require('querystring');
 var fs = require('fs');
 
-var React = require('react');
-var Redux = require('redux');
-var ReactRedux = require('react-redux');
+//var Redux = require('redux');
+//var ReactRedux = require('react-redux');
 
-var com = require('./ui.js');
-var renderToString = require('react-dom/server').renderToString;
 var TemplateRouter = require('uri-template-router');
 
-var port = process.env.PORT || 8080;
+var RouteDocument = require('./lib/route-document.js');
+//var RouteStatic = require('./lib/route-static.js');
+
+var listenPort = process.env.PORT || 8080;
+var fixedScheme = 'http';
+var fixedAuthority = 'localhost';
 
 var server = http.createServer(handleRequest);
-server.listen(port);
-console.log('Server running at http://127.0.0.1:' + port + '/');
+server.listen(listenPort);
+console.log('Server running at http://127.0.0.1:' + listenPort + '/');
 
 var routes = new TemplateRouter.Router();
 routes.addTemplate('http://localhost/{?instance}', null, new RouteDocument());
@@ -23,49 +26,31 @@ routes.addTemplate('http://localhost/style.css', null, new RouteStatic('style.cs
 
 function handleRequest(req, res){
 	if(typeof req.url!='string') throw new Error('Expected request.url to be a string');
+	// Construct effective request URI
+	// <https://tools.ietf.org/html/rfc7230#section-5.5>
+	// var port
 	if(req.url[0]=='/'){
-		var uri = 'http://localhost'+req.url;
+		var host = fixedAuthority || req.headers['host'];
+		var euri = fixedScheme+'://'+host+req.url;
+	}else if(req.url==='*'){
+		// Make the server talk about itself
+		var euri = 'http://localhost/';
 	}else{
-		var uri = req.url;
+		var euri = req.url;
 	}
-	var route = routes.resolveURI(uri);
-	console.log('Request: '+uri, route);
+	var queryOffset = euri.indexOf('?');
+	var uriHier = euri;
+	if(queryOffset >= 0){
+		var uriHier = euri.substring(0, queryOffset);
+		var uriQuery = euri.substring(queryOffset+1);
+	}
+	var queryMap = qs.parse(uriQuery);
+	var route = routes.resolveURI(uriHier);
+	console.log('Request: '+euri, route);
 	if(route && route.name && typeof route.name.process=='function'){
-		return void route.name.process(req, res);
+		return void route.name.process(req, res, route, euri, queryMap);
 	}
-	handleRequestNotFound(req, res, uri);
-}
-
-
-function RouteDocument(name){
-	this.name = name;
-}
-RouteDocument.prototype.process = function handleRequestDocument(req, res){
-	var state = com.initialState();
-	var props = {
-		state: state,
-		onLoadDocument: function(){},
-	};
-	var apphtml = renderToString(React.createElement(com.ApplicationBody, props));
-	var html = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'
-		+ '<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><head>'
-		+ '<script type="application/ecmascript" crossorigin="" src="https://unpkg.com/react@16/umd/react.development.js"></script>'
-		+ '<script type="application/ecmascript" crossorigin="" src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>'
-		+ '<script type="application/ecmascript" crossorigin="" src="https://unpkg.com/redux-thunk@2.2.0/dist/redux-thunk.js"></script>'
-		+ '<script src="https://cdnjs.cloudflare.com/ajax/libs/redux/3.7.2/redux.js"></script>'
-		+ '<script src="https://cdnjs.cloudflare.com/ajax/libs/react-redux/5.0.6/react-redux.js"></script>'
-		+ '<script src="https://cdnjs.cloudflare.com/ajax/libs/immutable/3.8.1/immutable.js"></script>'
-		+ '<script type="application/ecmascript" src="ui.js"></script>'
-		+ '<script type="application/ecmascript">document.addEventListener("DOMContentLoaded", onLoad);</script>'
-		+ '<script type="application/json" id="state">'+com.serializeState(state).replace(/<\//g, '<\\/')+'</script>'
-		+ '<link rel="stylesheet" type="text/css" href="style.css" />'
-		+ '</head><body>'
-		+ '<div id="main">'+apphtml+'</div>'
-		+ '</body></html>';
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'application/xhtml+xml');
-	res.write(html);
-	res.end();
+	handleRequestNotFound(req, res, uriHier, queryMap);
 }
 
 function RouteStatic(filepath, mediatype){
@@ -83,10 +68,11 @@ RouteStatic.prototype.process = function handleRequestStatic(req, res){
 	});
 }
 
-function handleRequestNotFound(req, res, uri){
+function handleRequestNotFound(req, res, uri, param){
 	res.statusCode = 404;
 	res.setHeader('Content-Type', 'text/plain');
-	res.write('Page not found: '+uri);
+	res.write('Page not found: '+uri+'\n');
+	res.write(JSON.stringify(param)+'\n');
 	res.end();
 }
 
