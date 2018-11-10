9b629dd5e5d8e7c661bdd9e1fede7491bb66afe9
Austin Wright <aaa@bzfx.net>
2018-11-09 20:05:06 -0700

    Fix codemirror paths

--- http.app.js
+++ http.app.js
@@ -19,11 +19,10 @@ routes.addTemplate('http://localhost/', null, RouteDocument());
 //routes.addTemplate('http://localhost/jsonschemaparse.js', null, new RouteBrowserify('./lib/jsonschemaparse.js', 'JSONSchemaParse'));
 routes.addTemplate('http://localhost/ui.js', null, new RouteBrowserify('./ui.js'));
 routes.addTemplate('http://localhost/style.css', null, RouteStaticFile(__dirname, 'style/style.css', 'text/css'));
-routes.addTemplate('http://localhost/codemirror/lib/codemirror.css', null, RouteStaticFile(__dirname, 'codemirror/lib/codemirror.css', 'text/css'));
-routes.addTemplate('http://localhost/codemirror/addon/lint/lint.css', null, RouteStaticFile(__dirname, 'codemirror/addon/lint/lint.css', 'text/css'));
-routes.addTemplate('http://localhost/codemirror/lib/codemirror.js', null, RouteStaticFile(__dirname, 'codemirror/lib/codemirror.js', 'application/ecmascript'));
-routes.addTemplate('http://localhost/codemirror/mode/javascript/javascript.js', null, RouteStaticFile(__dirname, 'codemirror/mode/javascript/javascript.js', 'application/ecmascript'));
-routes.addTemplate('http://localhost/codemirror/addon/lint/lint.js', null, RouteStaticFile(__dirname, 'codemirror/addon/lint/lint.js', 'application/ecmascript'));
+
+routes.addTemplate('http://localhost/style/codemirror{/path*}.css', {}, RouteStaticFile(__dirname+'/codemirror', "{/path*}.css", 'text/css') );
+routes.addTemplate('http://localhost/style/codemirror{/path*}.js', {}, RouteStaticFile(__dirname+'/codemirror', "{/path*}.js", 'application/ecmascript') );
+
 routes.addTemplate('http://localhost/json-instance-lint.js', null, RouteStaticFile(__dirname, 'json-instance-lint.js', 'application/ecmascript'));
 
 //routes.addTemplate('http://localhost/jsonschemaparse.js', null, new RouteBrowserify('../parse/index.js', 'JSONSchemaParse'));
