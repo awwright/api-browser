68ccfc571765c9670409d40e9eb23625e98e74d0
Austin Wright <aaa@bzfx.net>
2018-11-09 23:56:24 -0700

    Fix RouteBrowserify

--- http.app.js
+++ http.app.js
@@ -15,10 +15,10 @@ const {
 
 var routes = new TemplateRouter.Router();
 routes.addTemplate('http://localhost/', null, RouteDocument());
-//routes.addTemplate('http://localhost/ui.js', null, RouteStaticFile(__dirname, 'ui.js', 'application/ecmascript'));
-//routes.addTemplate('http://localhost/jsonschemaparse.js', null, new RouteBrowserify('./lib/jsonschemaparse.js', 'JSONSchemaParse'));
-routes.addTemplate('http://localhost/ui.js', null, new RouteBrowserify('./ui.js'));
-routes.addTemplate('http://localhost/style.css', null, RouteStaticFile(__dirname, 'style/style.css', 'text/css'));
+
+routes.addTemplate('http://localhost/style/ui.js', null, RouteBrowserify('./ui.js'));
+routes.addTemplate('http://localhost/style{/path*}.js', {}, RouteStaticFile(__dirname+'/style', "{/path*}.js", 'application/ecmascript') );
+routes.addTemplate('http://localhost/style{/path*}.css', {}, RouteStaticFile(__dirname+'/style', "{/path*}.css", 'text/css') );
 
 routes.addTemplate('http://localhost/style/codemirror{/path*}.css', {}, RouteStaticFile(__dirname+'/codemirror', "{/path*}.css", 'text/css') );
 routes.addTemplate('http://localhost/style/codemirror{/path*}.js', {}, RouteStaticFile(__dirname+'/codemirror', "{/path*}.js", 'application/ecmascript') );
--- json-instance-lint.js
+++ json-instance-lint.js
@@ -11,30 +11,27 @@
   else // Plain browser env
     mod(CodeMirror);
 })(function(CodeMirror) {
-"use strict";
-
-CodeMirror.registerHelper("lint", "json", function(text) {
-	console.log(text);
-	var found = [];
-	if (!window.JSONSchemaParse) {
-		if (window.console) {
-			window.console.error("Error: window.JSONSchemaParse not defined, CodeMirror JSON linting cannot run.");
+	"use strict";
+	CodeMirror.registerHelper("lint", "json", function(text) {
+		console.log(text);
+		var found = [];
+		if (!window.JSONSchemaParse) {
+			if (window.console) {
+				window.console.error("Error: window.JSONSchemaParse not defined, CodeMirror JSON linting cannot run.");
+			}
+			return found;
+		}
+		try { var parse = JSONSchemaParse.parse(null, {charset:'string'}, text); }
+		catch(e) {
+			console.dir(e);
+			var loc = e.position || {};
+			found.push({
+				from: CodeMirror.Pos(loc.line-1, loc.column),
+				to: CodeMirror.Pos(loc.line-1, loc.column),
+				message: e.message
+			});
+			console.dir(parse);
 		}
 		return found;
-	}
-	
-	try { var parse = JSONSchemaParse.parse(null, {charset:'string'}, text); }
-	catch(e) {
-		console.dir(e);
-		var loc = e.position || {};
-		found.push({
-			from: CodeMirror.Pos(loc.line-1, loc.column),
-			to: CodeMirror.Pos(loc.line-1, loc.column),
-			message: e.message
-		});
-		console.dir(parse);
-	}
-	return found;
-});
-
+	});
 });
